fe851cefb0b1f2de0ba278547c81854d627d4a71
KamilloP <92442879+KamilloP@users.noreply.github.com>
2021-12-05 17:46:05 +0100

    Add files via upload

--- origami.ml
+++ origami.ml
@@ -1,75 +1,42 @@
-(*
-    Autor: Florian Ficek
-    Code Review: 
-*)
-
 open List;;
-
-
-(* ##### Deklaracja typów ###### *)
+(*typy*)
 type point = float * float;;
-
 type kartka = point -> int;;
-
-    (* deklaracja typu prostej (A,B,C) *)
-type prostaABC = float * float * float;;
-
-    (* margines błędów działania na floatach *)
-let eps = 1e-9;;
-
-
-(* ##### FUNKCJE POMOCNICZE ##### *)
-    (* funkcja wypisująca rónwanie prostej (A,B,C) otrzymanej z dwóch punktów (x1,y1),(x2,y2) *)
-let prosta ((x1,y1):point) ((x2,y2):point) =
-    let a = y1-.y2 and
-    b = x2-.x1 and
-    c = y2*.x1-.y1*.x2 in
-    (a,b,c)
-;;
-
-    (* funkcja wypisująca obraz punktu (x,y) względem prostej (A,B,C) *)
-let obraz ((x,y):point) ((a,b,c):prostaABC) = 
-    let nx = (-.(a*.a-.b*.b)*.x-.2.*.a*.b*.y-.2.*.a*.c)/.(a*.a+.b*.b)
-    and ny = ((a*.a-.b*.b)*.y-.2.*.a*.b*.x-.2.*.b*.c)/.(a*.a+.b*.b)
-    in (nx,ny)
-;;
-
-    (* funkcja sprawdzająca po której stronie prostej tworzonej przez dwa punkty (x1,y1),(x2,y2)
-        znajudje się punkt (p,q):
-        LEWA gdy w<0
-        ŚRODEK gdy w=0
-        PRAWA gdy w>0
-    *)
-let strona ((x1,y1):point) ((x2,y2):point) ((p,q):point) = (* lewa, prawa, środek *)
-    let w = (y2-.y1)*.(p-.x2)-.(q-.y2)*.(x2-.x1) in
-    if w = 0. then 0
-    else if w<0. then 1
-    else -1
-;;
-
-(* ##### FUNKCJE PROGRAMU ##### *)
-let prostokat ((x1,y1):point) ((x2,y2):point) =
-    fun (x,y) ->
-        if x1-.x<=eps && y1-.y<=eps && x-.x2<=eps && y-.y2<=eps then 1
-        else 0
-;;
-
-let kolko ((xs,ys):point) (r:float) = 
-    fun (x,y) ->
-        if (x-.xs)*.(x-.xs)+.(y-.ys)*.(y-.ys)-.(r*.r)<=eps then 1
-        else 0
-;;
-
-let zloz ((x1,y1):point) ((x2,y2):point) (k:kartka) = 
-    let (a,b,c) = prosta (x1,y1) (x2,y2) in
-    fun (x,y) -> 
-        let check = strona (x1,y1) (x2,y2) (x,y) in
-        if check = 0 then (k (x,y)) 
-        else if check = 1 then 
-            (k (x,y)) + k ((obraz (x,y) (a,b,c)))
-        else 0
-;;
-
-let skladaj (lista: (point*point) list ) (k:kartka) = 
-    List.fold_left (fun a (x,y) -> zloz x y a) k lista
-;;
+(*funkcje pomocnicze*)
+let square x = x *. x;;
+let vector p1 p2 = (fst p2 -. fst p1, snd p2 -. snd p1);;
+let add_vec (x, y) (a, b) = (x +. a, y +. b);;
+let multi_vec (a, b) c = (a *. c, b *. c);;
+let vector_product a b = (fst a *. snd b) -. (snd a *. fst b);;
+let line_equation x (a, b) = (b, -1. *. a, (a *. snd x) -. (b *. fst x));; 
+(*oblicza rownanie prostej p1 p2 przechodzacej przez x,
+wynikiem jest krotka (c, d, e) taka, ze cx + dy + e = 0*)
+let intersection_of_lines (a, b, c) (d, e, f) = 
+  let x = ((b *. f) -. (c *. e)) /. ((a *. e) -. (b *. d)) 
+  and y = ((a *. f) -. (c *. d)) /. ((b *. d) -. (a *. e)) in
+  (x, y);;
+let opposite x p1 p2 = 
+  let (a, b) = vector p1 p2 in
+  let (c, d) = (-1. *. b, a) in
+  let vector_x_to_line_p1p2 = vector x (intersection_of_lines (line_equation p1 (a, b)) (line_equation x (c, d))) in
+  add_vec x (multi_vec vector_x_to_line_p1p2 2.);;
+(*konstruktory*)
+let prostokat ld pg = fun p -> if fst p <= fst pg && fst p >= fst ld && snd p <= snd pg && snd p >= snd ld then 1 else 0;;
+let kolko o r = fun p -> if sqrt (square (fst p -. fst o) +. square (snd p -. snd o)) <= r then 1 else 0;;
+(*modyfikatory*)
+let zloz p1 p2 k = fun x ->
+  let vec_pro = vector_product (vector p1 x) (vector p1 p2) in
+  if vec_pro = 0. then k x else (*x p1 p2 collinear*)
+  if vec_pro > 0. then 0 else (*x on right side*)
+  (+) (k x) (k (opposite x p1 p2));;
+let skladaj k lines = fold_left (fun acc (p1, p2) -> zloz p1 p2 acc) k lines;;
+(*
+let centr = (0., 0.);;
+let a = prostokat centr (10., 10.);;
+let a = zloz (5., 0.) (5., 377.) a;;
+let a = zloz (5., 0.) (5., 1.) a;;
+let b = zloz (-7., -7.) (300., 300.) a;;
+let c = zloz (-6., -6.) (-6.1, -6.1) a;;
+let d = zloz (9., 5.) (4., 2.) c;;
+assert(d (10., 3.) = 2);;
+*)
