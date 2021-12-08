19ab446654bb6979d8ae752c59873baf2a14a3fb
Florian Ficek <florke15@gmail.com>
2021-12-08 11:39:32 +0100

    after code review

--- origami.ml
+++ origami.ml
@@ -21,16 +21,16 @@ let eps = 1e-9;;
 (* ##### FUNKCJE POMOCNICZE ##### *)
     (* funkcja wypisująca rónwanie prostej (A,B,C) otrzymanej z dwóch punktów (x1,y1),(x2,y2) *)
 let prosta ((x1,y1):point) ((x2,y2):point) =
-    let a = y1-.y2 and
-    b = x2-.x1 and
-    c = y2*.x1-.y1*.x2 in
+    let a = y1 -. y2 and
+    b = x2 -. x1 and
+    c = y2 *. x1 -. y1 *. x2 in
     (a,b,c)
 ;;
 
     (* funkcja wypisująca obraz punktu (x,y) względem prostej (A,B,C) *)
 let obraz ((x,y):point) ((a,b,c):prostaABC) = 
-    let nx = (-.(a*.a-.b*.b)*.x-.2.*.a*.b*.y-.2.*.a*.c)/.(a*.a+.b*.b)
-    and ny = ((a*.a-.b*.b)*.y-.2.*.a*.b*.x-.2.*.b*.c)/.(a*.a+.b*.b)
+    let nx = ( -. (a *. a -. b *. b) *. x -. 2. *. a *. b *. y -. 2. *. a *. c) /. (a *. a +. b *. b)
+    and ny = ((a *. a -. b *. b) *. y -. 2. *. a *. b *. x -. 2. *. b *. c) /. (a *. a +. b *. b)
     in (nx,ny)
 ;;
 
@@ -41,7 +41,7 @@ let obraz ((x,y):point) ((a,b,c):prostaABC) =
         PRAWA gdy w>0
     *)
 let strona ((x1,y1):point) ((x2,y2):point) ((p,q):point) = (* lewa, prawa, środek *)
-    let w = (y2-.y1)*.(p-.x2)-.(q-.y2)*.(x2-.x1) in
+    let w = (y2 -. y1) *. (p -. x2) -. (q -. y2) *. (x2 -. x1) in
     if w = 0. then 0
     else if w<0. then 1
     else -1
@@ -50,20 +50,20 @@ let strona ((x1,y1):point) ((x2,y2):point) ((p,q):point) = (* lewa, prawa, środ
 (* ##### FUNKCJE PROGRAMU ##### *)
 let prostokat ((x1,y1):point) ((x2,y2):point) =
     fun (x,y) ->
-        if x1-.x<=eps && y1-.y<=eps && x-.x2<=eps && y-.y2<=eps then 1
+        if x1 -. x <= eps && y1 -. y <= eps && x -. x2 <= eps && y -. y2 <= eps then 1
         else 0
 ;;
 
 let kolko ((xs,ys):point) (r:float) = 
     fun (x,y) ->
-        if (x-.xs)*.(x-.xs)+.(y-.ys)*.(y-.ys)-.(r*.r)<=eps then 1
+        if (x -. xs) *. (x -. xs) +. (y -. ys) *. (y -. ys) -. (r *. r) <= eps then 1
         else 0
 ;;
 
-let zloz ((x1,y1):point) ((x2,y2):point) (k:kartka) = 
-    let (a,b,c) = prosta (x1,y1) (x2,y2) in
+let zloz ((p1):point) ((p2):point) (k:kartka) = 
+    let (a,b,c) = prosta p1 p2 in
     fun (x,y) -> 
-        let check = strona (x1,y1) (x2,y2) (x,y) in
+        let check = strona p1 p2 (x,y) in
         if check = 0 then (k (x,y)) 
         else if check = 1 then 
             (k (x,y)) + k ((obraz (x,y) (a,b,c)))
